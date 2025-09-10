1d00af4ae7230c94f3b6b4363cd73c48e44a5021
Admin <divyanshu.singh.bzzf@gmail.com>
2025-09-10 10:10:50 +0530

    Handling preflight

--- server.js
+++ server.js
@@ -26,6 +26,11 @@ app.use(cors({
     origin: "https://e-com-front-sage.vercel.app",
     credentials: true
 }));
+app.options('*', cors({
+  origin: "https://e-com-front-sage.vercel.app",
+  credentials: true
+}));
+
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
